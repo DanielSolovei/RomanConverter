e05d96b8698c225c4d1b0f5d33794aac592e353d
Daniil Solovev <dan-nighgtingake@yandex.ru>
2020-12-15 06:50:21 +0300

    Project added

---
+++ Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace NumbConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.bRomeConvert = new System.Windows.Forms.Button();
+            this.bArabkConvert = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // bRomeConvert
+            // 
+            this.bRomeConvert.BackColor = System.Drawing.SystemColors.Control;
+            this.bRomeConvert.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bRomeConvert.BackgroundImage")));
+            this.bRomeConvert.Font = new System.Drawing.Font("Algerian", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bRomeConvert.Location = new System.Drawing.Point(318, 146);
+            this.bRomeConvert.Name = "bRomeConvert";
+            this.bRomeConvert.Size = new System.Drawing.Size(223, 81);
+            this.bRomeConvert.TabIndex = 0;
+            this.bRomeConvert.Text = "CONVERT TO ROMAN NUMERALS";
+            this.bRomeConvert.UseVisualStyleBackColor = false;
+            this.bRomeConvert.Click += new System.EventHandler(this.bRomeConvert_Click);
+            // 
+            // bArabkConvert
+            // 
+            this.bArabkConvert.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bArabkConvert.BackgroundImage")));
+            this.bArabkConvert.Font = new System.Drawing.Font("Algerian", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bArabkConvert.Location = new System.Drawing.Point(318, 255);
+            this.bArabkConvert.Name = "bArabkConvert";
+            this.bArabkConvert.Size = new System.Drawing.Size(223, 81);
+            this.bArabkConvert.TabIndex = 1;
+            this.bArabkConvert.Text = "CONVERT TO ARABIC NUMERALS";
+            this.bArabkConvert.UseVisualStyleBackColor = true;
+            this.bArabkConvert.Click += new System.EventHandler(this.bArabConver_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox1.Font = new System.Drawing.Font("Algerian", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(30, 205);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(257, 49);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.Color.SandyBrown;
+            this.progressBar1.Location = new System.Drawing.Point(280, 393);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(302, 40);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox2.Font = new System.Drawing.Font("Algerian", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(577, 205);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(257, 49);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(883, 536);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.bArabkConvert);
+            this.Controls.Add(this.bRomeConvert);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Converter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bRomeConvert;
+        private System.Windows.Forms.Button bArabkConvert;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace NumbConverter
+{
+    public partial class Form1 : Form
+    {
+
+        private static int RomanToArabic(string roman)
+        {           
+            int i;
+            char ch;
+            int result = 0;
+            int new_value = 0;
+            int old_value = 0;
+            roman= roman.ToUpper();
+
+            for (i = 0; (i < roman.Length); i++)
+            {
+                ch = roman[i];
+
+                switch (ch)
+                {
+                    case 'I':
+                        new_value = 1;
+                        break;
+                    case 'V':
+                        new_value = 5;
+                        break;
+                    case 'X':
+                        new_value = 10;
+                        break;
+                    case 'L':
+                        new_value = 50;
+                        break;
+                    case 'C':
+                        new_value = 100;
+                        break;
+                    case 'D':
+                        new_value = 500;
+                        break;
+                    case 'M':
+                        new_value = 1000;
+                        break;
+                    default:                      
+                        throw new Exception("Введено неправильное значение");
+                }
+
+                if (new_value > old_value)
+                {
+                    result += new_value - 2 * old_value;
+                }
+                else
+                {
+                    result += new_value;
+                }
+
+                old_value = new_value;               
+            }
+
+            return result;
+        }
+
+        public static string ArabianToRome(int arabValue)
+        {
+       
+            string result = "";
+            int m1 = arabValue / 1000;
+            for (int i = 0; i < m1; i++)
+            {
+                //Console.Write("M");
+                result += "M";
+            }
+            int m2 = arabValue % 1000;
+            int d1 = m2 / 500;
+            if (m2 >= 900 && m2 < 1000)
+            {
+                result += "CM";
+                m2 -= 900;
+            }
+            else
+            {
+                for (int i = 0; i < d1; i++)
+                {
+                    //Console.Write("D");
+                    result += "D";
+
+                }
+            }
+            int d2 = m2 % 500;
+            int c1 = d2 / 100;
+            if (c1 == 4)
+            {
+                result += "CD";
+
+            }
+            else
+            {
+                for (int i = 0; i < c1 && i < 4; i++)
+                {
+                    //Console.Write("C");
+                    result += "C";
+                }
+            }
+            int c2 = d2 % 100;
+            int l1 = c2 / 50;
+            if (c2 >= 90 && c2 < 100)
+            {
+                result += "XC";
+                c2 -= 90;
+            }
+            else
+            {
+                for (int i = 0; i < l1; i++)
+                {
+                    //Console.Write("L");
+                    result += "L";
+                }
+            }
+            int l2 = c2 % 50;
+            int x1 = l2 / 10;
+            if (x1 == 4)
+            {
+                result += "XL";
+
+            }
+            else
+            {
+                for (int i = 0; i < x1; i++)
+                {
+                    //Console.Write("X");
+                    result += "X";
+                }
+            }
+            int x2 = l2 % 10;
+            int v1 = x2 / 5;
+            if (x2 == 9)
+            {
+                result += "IX";
+                x2 -= 9;
+            }
+            else
+            {
+                for (int i = 0; i < v1; i++)
+                {
+                    //Console.Write("V");
+                    result += "V";
+                }
+            }
+            int v2 = x2 % 5;
+            if (v2 == 4)
+            {
+                //Console.Write("IV");
+                result += "IV";
+            }
+            else
+            {
+                for (int i = 0; i < v2; i++)
+                {
+                    //Console.Write("I");
+                    result += "I";
+                }
+            }
+
+            return result;
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+       
+        //I = 1; V = 5; X = 10; L = 50; C = 100; D = 500; M = 1000.
+      
+        private void bRomeConvert_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                progressBar1.Value = i;
+            }
+
+            try
+            {              
+                textBox2.Text = ArabianToRome(int.Parse(textBox1.Text));
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Введено неправильное значение");
+                return;
+
+            } 
+        }
+
+        private void bArabConver_Click(object sender, EventArgs e)
+        {
+            for (int j = 0; j <= 100; j++)
+            {
+                progressBar1.Value = j;
+            }
+
+            try
+            {
+                textBox2.Text = RomanToArabic(textBox1.Text).ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return;
+            }          
+        }
+    }
+}
